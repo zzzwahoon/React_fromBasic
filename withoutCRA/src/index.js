1ac88bdc42674b2c17cd647ab538ad43a7ca8154
ZzzwaHoon <kkjw94@gmail.com>
2024-05-27 16:26:20 +0900

    withoutCRA - react,react-dom 세팅

---
+++ withoutCRA/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FollowButton from './components/FollowButton';
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<FollowButton />, domContainer);
