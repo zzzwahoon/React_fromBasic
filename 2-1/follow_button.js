4ebded41c077a39f99d32725a86c15484ef2651e
ZzzwaHoon <kkjw94@gmail.com>
2024-05-27 15:48:16 +0900

    HTML,CSS,JavaScript프로젝트에 리액트 추가

---
+++ 2-1/follow_button.js
@@ -0,0 +1,48 @@
+// 3단계: React 컴포넌트 만들기
+
+// React Component!
+// Follow <-> following
+// 스타터 코드
+function FollowButton() {
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "12px",
+    right: "16px",
+    cursor: "pointer",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold"
+  };
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "black",
+    color: "white"
+  };
+  const follwingBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid #cfd9de"
+  }
+
+  return React.createElement("div", 
+  {
+    onClick: () => {
+      setFollowing(!following);
+    },
+    style: following? follwingBtnStyle : followBtnStyle
+  },
+  following? "following" : "follow"
+);
+}
+
+const domContainer = document.querySelector('#follow_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(FollowButton));
+
