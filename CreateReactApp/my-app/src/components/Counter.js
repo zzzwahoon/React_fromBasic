177619585322d6e9cd98463a7e89e5666512e4dc
ZzzwaHoon <kkjw94@gmail.com>
2024-05-28 17:03:47 +0900

    state - 2. useState() hook

--- CreateReactApp/my-app/src/components/Counter.js
+++ CreateReactApp/my-app/src/components/Counter.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+// Hook 함수
 
 export default function Counter() {
-  let count = 0;
+  // 값, 값을 변경할 때 사용할 함수
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => (count += 1)}>+1</button> 
+      <button onClick={() => setCount(count + 1)}>+1</button> 
       <br />
       Counter : {count}
     </div>
