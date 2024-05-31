b8f08a8c3aadd70c71a425fe86614b4ccab4ab8d
ZzzwaHoon <kkjw94@gmail.com>
2024-05-31 16:37:42 +0900

    state - 7. 비동기로 이루어지는 setState

--- CreateReactApp/my-app/src/App.js
+++ CreateReactApp/my-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
 
 
 function App() {
   return (
-    <div class="App">
-      <Counter />
+    <div className="App">
+      <Counter2 />
     </div>
   )
 }
