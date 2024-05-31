f3279175610f9fff559187c4215b6127919f4762
ZzzwaHoon <kkjw94@gmail.com>
2024-05-31 16:53:16 +0900

    state - 10. state 끌어올리기 실습 (1)

--- CreateReactApp/my-app/src/App.js
+++ CreateReactApp/my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Counter from './components/Counter';
 import Counter2 from './components/Counter2';
-
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <Counter2 />
+      <br /><hr /><br />
+      <Calculator />
     </div>
   )
 }
