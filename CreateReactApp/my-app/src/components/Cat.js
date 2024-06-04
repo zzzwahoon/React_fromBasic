aca26b292c70c528b8f1f534465724f9c7c26f07
ZzzwaHoon <kkjw94@gmail.com>
2024-06-04 18:09:00 +0900

    리스트와 key / 폼과 이벤트 제어하기 - 13. ref 심화 (3) - callback ref

--- CreateReactApp/my-app/src/components/Cat.js
+++ CreateReactApp/my-app/src/components/Cat.js
@@ -1,6 +1,7 @@
-import React, { forwardRef, useEffect } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 
 const Cat = forwardRef((props, ref) => {
+  const [loaded, setLoaded] = useState(false);
   console.log('자식 컴포넌트 Cat');
   console.log(ref);
 
@@ -15,6 +16,11 @@ const Cat = forwardRef((props, ref) => {
         src="https://static01.nyt.com/images/2016/03/30/universal/ko/well_cat-korean/well_cat-superJumbo-v2.jpg?quality=90&auto=webp"
         alt="cat"
         style={{ width: '150px' }}
+        ref={loaded ? ref : () => undefined}
+        onLoad={() => {
+          alert('loaded');
+          setLoaded(true);
+        }}
       ></img>
     </div>
   );
--- CreateReactApp/my-app/src/components/CatParent.js
+++ CreateReactApp/my-app/src/components/CatParent.js
@@ -4,28 +4,34 @@ import Cat from './Cat';
 // Cat 안에 있는 image 사이즈를 알고싶다
 export default function CatParent() {
   const catRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  const catCallbackRef = (node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  };
 
   console.log('Parent component CatParent');
-  console.log(catRef);
+  // console.log(catRef);
 
   const ageRef = useRef(1);
-  const [state, setState] = useState(1);
-  console.log(ageRef.current);
+  // console.log(ageRef.current);
 
   return (
     <div>
       <h4> 고양이가 세상을 구한다 </h4>
       <div>
-        <Cat a={'a'} ref={catRef} />
+        <Cat a={'a'} ref={catCallbackRef} />
         <h3>age: {ageRef.current}</h3>
-        <h4>State: {state}</h4>
-        <button onClick={() => alert(catRef.current.height)}>
+        <h4>height: {height}</h4>
+        {/* <button onClick={() => alert(catRef.current.height)}>
           고양이 크기를 알고 싶어
-        </button>
+        </button> */}
         <button onClick={() => (ageRef.current = ageRef.current + 1)}>
           age Up
         </button>
-        <button onClick={() => setState(state + 1)}>test</button>
+        {/* <button onClick={() => setState(state + 1)}>test</button> */}
       </div>
     </div>
   );
