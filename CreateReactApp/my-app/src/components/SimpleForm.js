6bd61aa9b217e23e7b13bd8447a48757e4321665
ZzzwaHoon <kkjw94@gmail.com>
2024-06-04 15:42:05 +0900

    리스트와 key / 폼과 이벤트 제어하기 - 08. 다중 입력 다루기

--- CreateReactApp/my-app/src/components/SimpleForm.js
+++ CreateReactApp/my-app/src/components/SimpleForm.js
@@ -1,28 +1,46 @@
 import React, { useState } from 'react';
 
 export default function SimpleForm() {
-  const [username, setUsername] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const [userInputs, setUserInputs] = useState({
+    username: '',
+    password: '',
+  });
 
   const handleChange = (e) => {
-    setUsername(e.target.value);
+    setUserInputs({ ...userInputs, [e.target.name]: e.target.value })
+    // if (e.target.name === 'username') return setUsername(e.target.value);
+    // return setPassword(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(username);
+    const { username, password } = userInputs;
+    alert(`username : ${username}, password : ${password}`);
   };
 
   // e.target.value ?
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>User name: </label>
+      <label>Username : </label>
       <input
         type="text"
-        name="user name"
+        name="username"
         onChange={handleChange}
-        value={username}
+        value={userInputs.username}
       />
+      <br />
+      <label>Password : </label>
+      <input
+        type="text"
+        name="password"
+        onChange={handleChange}
+        value={userInputs.password}
+      />
+      <br />
       <input type="submit" value="submit" />
     </form>
   );
